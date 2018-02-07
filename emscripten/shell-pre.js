48264c0770553ff1f788dd65614a1f783a511afd
wothke <tinyrsid@wothke.ch>
2018-02-07 22:39:50 +0100

    updates for potential use of WebAssembly

--- emscripten/shell-pre.js
+++ emscripten/shell-pre.js
@@ -1,4 +1,12 @@
 // create separate namespace for all the Emscripten stuff.. otherwise naming clashes may occur especially when 
 // optimizing using closure compiler..
-window.spp_backend_state_XMP= {};
+window.spp_backend_state_XMP= {
+	notReady: true,
+	adapterCallback: function(){}	// overwritten later	
+};
+window.spp_backend_state_XMP["onRuntimeInitialized"] = function() {	// emscripten callback needed in case async init is used (e.g. for WASM)
+	this.notReady= false;
+	this.adapterCallback();
+}.bind(window.spp_backend_state_XMP);
+
 var backend_XMP = (function(Module) {
